c7d320b1f31485e80e4a243fe476e079d4cbe4e5
mohamedwaziri2006-dotcom <mohamedwaziri2006@gmail.com>
2026-09-25 10:55:07 +0300

    Add Service Worker for PWA

--- script.js
+++ script.js
@@ -790,3 +790,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchGoals();
   }
 });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(() => console.log('Service Worker Registered'));
+}
